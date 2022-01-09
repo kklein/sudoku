bacdd90710a388d5be76386b6efc78962b6b37b7
kklein <kleinkevin.lu@gmail.com>
2022-01-09 15:02:52 +0100

    Fix naming of file.

--- rl_gym/train.py
+++ rl_gym/train.py
@@ -1,5 +1,4 @@
 from stable_baselines3 import DQN
-
 from sudoku_env import SudokuEnv
 
 N_BLOCKS = 3
@@ -9,7 +8,7 @@ env = SudokuEnv(n_blocks=N_BLOCKS)
 
 trained_model = DQN("MlpPolicy", env, verbose=1)
 
-file_name = f"sudoku_{N_BLOCKS}_{N_TIMESTEPS}"
+file_name = f"sudoku_{N_BLOCKS}_{N_TIMESTEPS/1_000_000}M"
 
 trained_model.learn(total_timesteps=N_TIMESTEPS)
 trained_model.save(file_name)
